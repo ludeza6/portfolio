fea410e5f25bdda857400ccc4f86d0d3a83cd706
ludeza6 <ludeza6@gmail.com>
2026-07-27 20:54:48 -0400

    create app function to save current world states

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Background from "./components/Background"
 import Sidebar from "./components/Sidebar"
 import Navigation from "./components/Navigation"
 import AboutWorld from "./worlds/AboutWorld"
+import { useState } from "react"
 
 function App() {
+  const [currentWorld, setCurrentWorld] = useState(1)
   return (
     <>
       <Background />
