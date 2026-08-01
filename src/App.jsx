be7ba71c8a8665f2add1743a64fb3955f45366cd
ludeza6 <ludeza6@gmail.com>
2026-07-31 22:16:18 -0400

    update world counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import Background from "./components/Background"
 import Sidebar from "./components/Sidebar"
 import Navigation from "./components/Navigation"
 import AboutWorld from "./worlds/AboutWorld"
+
 import { useState } from "react"
+
+import ProjectsWorld from "./worlds/ProjectsWorld"
+import SkillsWorld from "./worlds/SkillsWorld"
+import ExperienceWorld from "./worlds/ExperienceWorld"
+import ContactWorld from "./worlds/ContactWorld"
+
 
 function App() {
   const [currentWorld, setCurrentWorld] = useState(1)
@@ -10,10 +17,18 @@ function App() {
     <>
       <Background />
       <Sidebar />
-      <Navigation />
-      <AboutWorld />
+      <Navigation
+        currentWorld={currentWorld}
+        setCurrentWorld={setCurrentWorld}
+      />
+
+      {currentWorld === 1 && <AboutWorld />}
+      {currentWorld === 2 && <ProjectsWorld />}
+      {currentWorld === 3 && <SkillsWorld />}
+      {currentWorld === 4 && <ExperienceWorld />}
+      {currentWorld === 5 && <ContactWorld />}
     </>
-  )
+  );
 }
 
 export default App
