4b1537837baa27c30e91a7da1ae611677d46ff72
ludeza6 <ludeza6@gmail.com>
2026-08-03 12:36:28 -0400

    change worlds into arrays of objs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import Background from "./components/Background"
 import Sidebar from "./components/Sidebar"
 import Navigation from "./components/Navigation"
-import AboutWorld from "./worlds/AboutWorld"
-
 import { useState } from "react"
-
-import ProjectsWorld from "./worlds/ProjectsWorld"
-import SkillsWorld from "./worlds/SkillsWorld"
-import ExperienceWorld from "./worlds/ExperienceWorld"
-import ContactWorld from "./worlds/ContactWorld"
+import worlds from "./data/worlds"
 
 
 function App() {
+
   const [currentWorld, setCurrentWorld] = useState(1)
+  const currentWorldData = worlds[currentWorld - 1]
+  const CurrentWorld = currentWorldData.component
+
   return (
     <>
       <Background />
@@ -21,12 +19,7 @@ function App() {
         currentWorld={currentWorld}
         setCurrentWorld={setCurrentWorld}
       />
-
-      {currentWorld === 1 && <AboutWorld />}
-      {currentWorld === 2 && <ProjectsWorld />}
-      {currentWorld === 3 && <SkillsWorld />}
-      {currentWorld === 4 && <ExperienceWorld />}
-      {currentWorld === 5 && <ContactWorld />}
+      <CurrentWorld />
     </>
   );
 }
--- src/data/worlds.js
+++ src/data/worlds.js
@@ -1,19 +1,30 @@
+import AboutWorld from "../worlds/AboutWorld";
+import ProjectsWorld from "../worlds/ProjectsWorld";
+import SkillsWorld from "../worlds/SkillsWorld";
+import ExperienceWorld from "../worlds/ExperienceWorld";
+import ContactWorld from "../worlds/ContactWorld";
+
 const worlds = [
   {
     name: "About Me",
+    component: AboutWorld
   },
   {
     name: "Projects",
+    component: ProjectsWorld
   },
   {
     name: "Skills",
+    component: SkillsWorld
   },
   {
     name: "Experience",
+    component: ExperienceWorld
   },
   {
     name: "Contact",
+    component: ContactWorld
   },
 ];
 
-export default worlds
+export default worlds;
