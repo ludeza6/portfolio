312fdda6c7313926f9fdc79eb618bf4a00d6f0f8
ludeza6 <ludeza6@gmail.com>
2026-08-02 20:13:46 -0400

    create world swipe right button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,13 @@
 function Navigation(props) {
-  return <div> {props.currentWorld};
+  return (
+    <div>
+      {props.currentWorld}
 
-  <button onClick={()}
+      <button onClick={() => props.setCurrentWorld(props.currentWorld + 1)}> ➡️ </button>
+    </div>
+  )
+
+  
   
 }
 
