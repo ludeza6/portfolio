bd654d9417ff17b291bbc3bbacf7e3460135ad31
ludeza6 <ludeza6@gmail.com>
2026-08-06 19:16:14 -0400

    create world 1 component CSS structure

--- src/worlds/AboutWorld.jsx
+++ src/worlds/AboutWorld.jsx
@@ -1,5 +1,14 @@
 function AboutWorld() {
-  return <section className="world about-world">AboutWorld</section>;
+  return (
+    <section className="world about-world">
+
+      <div className="island">
+        <div className="island-grass"></div>
+        <div className="island-dirt"></div>
+      </div>
+
+    </section>
+  )
 }
 
 export default AboutWorld;
