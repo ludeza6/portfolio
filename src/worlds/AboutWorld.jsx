7f4133a65bd58d009364f81940edd0e6b582f6d2
ludeza6 <ludeza6@gmail.com>
2026-08-17 22:48:36 -0400

    fix aboutworlddisplay

--- src/worlds/AboutWorld.jsx
+++ src/worlds/AboutWorld.jsx
@@ -6,12 +6,30 @@ function AboutWorld() {
       <div className="cloud cloud-one"></div>
       <div className="cloud cloud-two"></div>
 
+      <div className="about-panel">
+        <p className="about-label">PLAYER PROFILE</p>
+        <h1>Hi, I'm Lucas.</h1>
+        <p>
+          CSE student building software, AI systems,
+          and interactive projects.
+        </p>
+      </div>
+
       <div className="island">
-        <div className="tree tree-left"></div>
+        <div className="tree tree-left">
+          <div className="tree-crown"></div>
+          <div className="tree-trunk"></div>
+        </div>
 
-        <div className="character"></div>
+        <div className="character">
+          <div className="character-head"></div>
+          <div className="character-body"></div>
+        </div>
 
-        <div className="tree tree-right"></div>
+        <div className="tree tree-right">
+          <div className="tree-crown"></div>
+          <div className="tree-trunk"></div>
+        </div>
 
         <div className="island-grass"></div>
 
