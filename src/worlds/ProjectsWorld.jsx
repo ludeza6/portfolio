501da2a2bf1d66a3c65e961bd3a1955b2af95b6a
ludeza6 <ludeza6@gmail.com>
2026-08-24 11:43:37 -0400

    add project panel buttons and descriptions

--- src/worlds/ProjectsWorld.jsx
+++ src/worlds/ProjectsWorld.jsx
@@ -1,18 +1,51 @@
 import "./ProjectsWorld.css";
 import projects from "../data/projects"
+import { useState } from "react";
 
 function ProjectsWorld() {
+
+  const [selectedProject, setSelectedProject] = useState(null);
+
   return (
     <section className="world projects-world">
+      <div className="projects-heading">
+        <span>WORLD 2</span>
+        <h1>PROJECTS</h1>
+        <p>Select an iceberg to inspect a project.</p>
+      </div>
+
       <div className="ice-field">
         {projects.map((project) => (
-          <div className="iceberg" key={project.title}>
+          <button
+          type="button"
+            className={
+              selectedProject === project
+                ? "iceberg iceberg-selected"
+                : "iceberg"
+            }
+            key={project.title}
+            onClick={() => setSelectedProject(project)}
+          >
             <div className="project-icon">{project.icon}</div>
 
             <div className="project-title">{project.title}</div>
-          </div>
+          </button>
         ))}
       </div>
+
+      {selectedProject && (
+        <div className="project-panel">
+          <h2>{selectedProject.title}</h2>
+
+          <p>{selectedProject.description}</p>
+
+          <button
+          type="button" 
+          onClick={() => setSelectedProject(null)}>CLOSE
+          </button>
+        </div>
+      )}
+
     </section>
   );
 
